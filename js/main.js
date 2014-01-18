7cc5a87d422469c330aca8d1e40cfc320f27967c
viniciuspalma <vini_palma@outlook.com>
2014-01-18 03:54:12 -0200

    Alteração no layout
    Adição do touch-punch, novo BG.

--- js/main.js
+++ js/main.js
@@ -14,11 +14,12 @@ $(function() {
         myLatLng = latlng;
         var mapOptions = {
             center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            zoom:7,
+            mapTypeId: google.maps.MapTypeId.SATELLITE,
             disableDefaultUI: true,
         };
 
-        if (boolTripTrack==true)
+        if (boolTripTrack===true)
         {
             _map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
         }
@@ -34,11 +35,10 @@ $(function() {
     currentLatitude = "-29,41762",
     currentLongitude = "-51,14057",
     options = {
-        timeout: 1000,
+        timeout: 15000,
         maximumAge: 11000,
         enableHighAccuracy: true
     },
-        
     initialize=function(){
         rowIn.remove();
         inicio.css( "height", $( window ).height() );
@@ -49,8 +49,16 @@ $(function() {
         writeDiv.css( "height", $( window ).height() );
         mapDiv.css( "height", $( window ).height() );
         mpwrDiv.css( "height", $( window ).height() );
-        mpwrDiv.show( "slow" );
+        mpwrDiv.show();
+        userDataVideo();
+        
     },
+    userDataVideo=function(){        
+        navigator.getUserMedia('video, audio', function(localMediaStream) {
+            var video = document.querySelector('video');
+            video.src = window.URL.createObjectURL(localMediaStream);
+        });
+    },    
     getLocation = function(){
         console.log("in getLocation",4);
     },
@@ -59,7 +67,7 @@ $(function() {
 	},
     suc = function(p){
 		console.log("geolocation success",4);
-		if( _map == null ) {
+		if( _map === null ) {
 			currentLatitude = p.coords.latitude;
 			currentLongitude = p.coords.longitude;
 			drawMap();
@@ -74,7 +82,7 @@ $(function() {
               map: _map
             });
             
-            if( _llbounds == null ){
+            if( _llbounds === null ){
                 _llbounds = new google.maps.LatLngBounds(new google.maps.LatLng(p.coords.latitude, p.coords.longitude));//original
             }
             else{
@@ -88,67 +96,27 @@ $(function() {
     onDeviceReady=function(){
         try
         {
-            if (intel.xdk.device.platform.indexOf("Android")!=-1)
-            {
-                intel.xdk.display.useViewport(480,480);
-                document.getElementById("map_canvas").style.width="480px";
-            }
-            else if (intel.xdk.device.platform.indexOf("iOS")!=-1)
-            {
-                if (intel.xdk.device.model.indexOf("iPhone")!=-1 || intel.xdk.device.model.indexOf("iPod")!=-1)
-                {
-                    intel.xdk.display.useViewport(320,320);
-                    document.getElementById("map_canvas").style.width="320px";
-                }
-                else if (intel.xdk.device.model.indexOf("iPad")!=-1)
-                {
-                    intel.xdk.display.useViewport(768,768);
-                    document.getElementById("map_canvas").style.width="768px";
-                }
-            }
-            
-           if (intel.xdk.iswin8) {
-                document.getElementById("map_canvas").style.width = screen.width + "px"
-                document.getElementById("map_canvas").style.height = screen.height + "px";
-            }
-
-            
-			if (intel.xdk.geolocation != null)
-			{
+			if (intel.xdk.geolocation !== null){
 				document.getElementById("map_canvas").style.height = screen.height + "px";
 				intel.xdk.geolocation.watchPosition(suc,fail,options);
 			}
         }
-        catch(e)
-        {
+        catch(e){
             alert(e.message);
         }
-
-        try
-        {
-            //lock orientation
-            intel.xdk.device.setRotateOrientation("portrait");
-            intel.xdk.device.setAutoRotate(false);
-        }
-        catch(e) {}
-
-        try
-        {
-            //manage power
-            intel.xdk.device.managePower(true,false);
-        }
-        catch(e) {}
 		
-		try
-		{
+		try{
 			//hide splash screen
 			intel.xdk.device.hideSplashScreen();
             setTimeout(initialize, 5000);
+            
         }
         catch(e) {}
     };    
     
     document.addEventListener("intel.xdk.device.ready",onDeviceReady,false);
-    document.querySelector( '.btn-write' ).addEventListener("click", initializeMap);
+    document.querySelector( '.btn-write' ).addEventListener ( "click", initializeMap );
+    
+    
     $(document).foundation();
 });
